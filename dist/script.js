cec5ac0f5f2dbaec7382162e9cdda21c63fbd5b3
renan-peixoto <renan2.peixoto@gmail.com>
2022-09-09 15:56:32 -0300

    add euro

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,25 @@
-function converter() {
-  const valorDoElemento = document.getElementById("valor");
+function converterDolar() {
+  const valorDoElemento = document.getElementById("valorDolar");
   const valor = valorDoElemento.value;
   const valorEmDolarNumerico = parseFloat(valor);
-  const valorEmReal = valorEmDolarNumerico * 5;
+  const valorEmReal = valorEmDolarNumerico * 5.15;
   console.log(valorEmReal);
   
   // imprimir na tela
-  const elementoValorConvertido = document.getElementById("valorConvertido");
-  const valorConvertido = `O valor em dólar é: ${valorEmReal}`;
+  const elementoValorConvertido = document.getElementById("valorConvertidoDolar");
+  const valorConvertido = `O valor em dólar é: US$ ${valorEmReal}`;
+  elementoValorConvertido.innerHTML = valorConvertido;
+}
+
+function converterEuro() {
+  const valorDoElemento = document.getElementById("valorEuro");
+  const valor = valorDoElemento.value;
+  const valorEmDolarNumerico = parseFloat(valor);
+  const valorEmReal = valorEmDolarNumerico * 5.17;
+  console.log(valorEmReal);
+  
+  // imprimir na tela
+  const elementoValorConvertido = document.getElementById("valorConvertidoEuro");
+  const valorConvertido = `O valor em uro é: ${valorEmReal} €`;
   elementoValorConvertido.innerHTML = valorConvertido;
 }
